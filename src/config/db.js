fe7c8dfe46968fb139a6a02189c3d3ccefc2fef0
SUHO PARK <duglikespiano@gmail.com>
2026-03-06 19:50:57 +0900

    FEATURE - SEED FILE ADDED

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,8 @@ import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
 dotenv.config({ path: './src/.env' });
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const databaseURL = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString: databaseURL });
 const adapter = new PrismaPg(pool);
 const nodeEnv = process.env.NODE_ENV;
 
